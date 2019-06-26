13cc22b43ba69335be5d22e62f2d7413384808e1
echupeko <echupeko@gmail.com>
2019-06-26 20:50:41 +0700

    Add files via upload

---
+++ other/CardGame/script/script.js
@@ -0,0 +1,11 @@
+const listCard = [
+    'C6','C7','C8','C9','C10','CJack','CQueen','CKing','CAce',
+    'D6','D7','D8','D9','D10','DJack','DQueen','DKing','DAce',
+    'H6','H7','H8','H9','H10','HJack','HQueen','HKing','HAce',
+    'S6','S7','S8','S9','S10','SJack','SQueen','SKing','SAce',
+];
+
+
+randomGetCard = () => {
+
+}
