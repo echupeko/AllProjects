934f5030025ee05569e104649c9e388f2146a245
Евгений <echupeko@gmail.com>
2019-06-27 14:15:45 +0700

    update Card

--- other/CardGame/script/script.js
+++ other/CardGame/script/script.js
@@ -1,3 +1,6 @@
+const tableGame = document.getElementById('tableBlock');
+let imgBlock = tableGame.querySelectorAll('img');
+
 const listCard = [
     'C6','C7','C8','C9','C10','CJack','CQueen','CKing','CAce',
     'D6','D7','D8','D9','D10','DJack','DQueen','DKing','DAce',
@@ -7,5 +10,36 @@ const listCard = [
 
 
 randomGetCard = () => {
+let valueGet = null;
+let max = listCard.length;
+    let arr = getRandomArray(max, 6);
+    let arr2 = arr.slice(0);
+    for (let i = 0; i < arr.length; i++){
+        imgBlock[i].src = 'resources/' + listCard[arr[i]] + '.png';
+    }
+    for (let i = 0; i < arr2.length; i++){
+            imgBlock[i+6].src = 'resources/' + listCard[arr2[i]] + '.png';
+    }
+
 
 }
+
+function getRandomArray(max = 0, length = 0){
+    let parseLength = Number.parseInt(length.toString());
+    let parseMax = Number.parseInt(max.toString());
+
+    if (Number.isNaN(parseLength) || Number.isNaN(parseMax)) return [];
+    if (parseLength < 0 || parseMax < 0 || parseLength >= parseMax) return [];
+
+    let current, array = [];
+
+    while (array.length !== parseLength) {
+        current = Math.floor(Math.random() * parseMax);
+        if (!array.includes(current)) {
+            array.push(current);
+        }
+    }
+    return array;
+}
+
+randomGetCard();
