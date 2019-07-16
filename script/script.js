06f750ef17fef3212a86c0733be27c0118fa7bf5
Евгений <echupeko@gmail.com>
2019-07-16 15:36:30 +0700

    update Card

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
 let id = 1;
 menuBar = () => {
     const menu = document.getElementById('menu');
-    if (window.matchMedia('(max-width: 360px)').matches) {
+    if (window.matchMedia('(max-width: 550px)').matches) {
         if (id) {
-            menu.style.display = 'block';
+            menu.classList.add('active');
             id--;
         }
         else {
-            menu.style.display = 'none';
+            menu.classList.toggle('active');
             id++;
         }
     }
