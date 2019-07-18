b4c0f45e6c4daf528f34e88d295fd422befbb505
Евгений <echupeko@gmail.com>
2019-07-18 16:42:49 +0700

    update Card

---
+++ script/script.js
@@ -0,0 +1,23 @@
+/*Добавить возможность открытия раздела
+* выбранный раздел отображается слева
+* скроллинг при помощи ведения мыши
+* */
+let wDelta = 120;
+
+scrollDoc = (e) => {
+    var __delta = e.wheelDelta || -e.detail;
+    __delta /= Math.abs(__delta);
+    document.documentElement.scrollLeft -= __delta * wDelta; // FF, Opera, IE
+    if (this.attachEvent) return false;
+    document.body.scrollLeft -= __delta * wDelta; // Chrome
+}
+    window.onload = function () {
+    let html = document.documentElement;
+    if (html.attachEvent) {
+        html.attachEvent("onmousewheel", scrollDoc); // IE and Opera
+    } else {
+        html.addEventListener("DOMMouseScroll", scrollDoc, false); // FF
+        html.addEventListener("mousewheel", scrollDoc, false); // Chrome
+    }
+
+}
