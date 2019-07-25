ef7876762b1dea1efc4595cb8a1545a60f5bfad0
Евгений <echupeko@gmail.com>
2019-07-25 11:59:54 +0700

    update Card

--- script/script.js
+++ script/script.js
@@ -11,10 +11,44 @@ const contentArr = [
     {id: 'hobby', description: 'увлечения'},
     {id: 'contact', description: 'контакты'}];
 const descriptionArr = [
-    {id: 'javascript', pattern: 'skills', p: null}
+    {id: 'javascript', pattern: 'skills', p: null},
+    {id: 'html', pattern: 'skills', p: null},
+    {id: 'css', pattern: 'skills', p: null},
+    {id: '1C', pattern: 'skills', p: null},
+    {id: 'asu2015', pattern: 'education', p: null},
+    {id: 'asu2017', pattern: 'education', p: null},
+    {id: 'cardgame', pattern: 'development', p: null},
+    {id: 'psh', pattern: 'development', p: null},
+    {id: 'constructor', pattern: 'development', p: null},
+    {id: 'bicycle', pattern: 'hobby', p: null},
+    {id: 'snowboard', pattern: 'hobby', p: null},
+    {id: 'photoshop', pattern: 'hobby', p: null},
+    {id: 'needlework', pattern: 'hobby', p: null},
+    {id: 'adress', pattern: 'contact', p: null},
+    {id: 'email', pattern: 'contact', p: null},
+    {id: 'socialnetwork', pattern: 'contact', p: null},
 ]
 const descriptionTextArr = [
-    {id: 'javascript', skillLevel: 'Начинающий', description: 'Занимаюсь изучением JavaScript'}
+    {id: 'javascript', skillLevel: 'Начинающий', description: 'Занимаюсь изучением JavaScript'},
+    {id: 'html', skillLevel: 'Начинающий', description: 'Занимаюсь изучением HTML'},
+    {id: 'css', skillLevel: 'Начинающий', description: 'Занимаюсь изучением CSS'},
+    {id: '1C', skillLevel: 'Продвинутый', description: 'Работаю на 1С 7.7'},
+    {id: 'asu2015', skillLevel: 'Бакалавр', description: 'В 2015 году получил степень бакалавра в ' +
+        'Алтайском государственном университете'},
+    {id: 'asu2017', skillLevel: 'Магистр', description: 'В 2017 получил степень магистра в ' +
+        'Алтайском государственном университете с красным дипломом'},
+    {id: 'cardgame', skillLevel: 'Начинающий', description: 'Игра на развитие зрительной памяти'},
+    {id: 'psh', skillLevel: 'Начинающий', description: 'Сайт-галлерея, для демонстрации работ иллюстратора'},
+    {id: 'constructor', skillLevel: 'Начинающий', description: 'Coming soon'},
+    {id: 'bicycle', skillLevel: 'Опытный', description: 'Очень нравятся прогулки на велосипеде'},
+    {id: 'snowboard', skillLevel: 'Начинающий', description: 'Увлекаюсь спусками с горы на сноуборде'},
+    {id: 'photoshop', skillLevel: 'Начинающий', description: 'Занимаюсь изучением Adobe Photoshop, ' +
+        'люблю создавать необычные работы'},
+    {id: 'needlework', skillLevel: 'Начинающий', description: 'Работа с деревом и металлом успокаивает, ' +
+        'а так же предоставляет возможность сделать что-то своими руками'},
+    {id: 'adress', skillLevel: 'Начинающий', description: 'Алтайский край, г. Барнаул'},
+    {id: 'email', skillLevel: 'Начинающий', description: 'email: echupeko@gmail.com'},
+    {id: 'socialnetwork', skillLevel: 'Начинающий', description: 'vk instagram whatsapp skype'}
 ]
 const panelArr = [];
 let isClicked = false;
@@ -26,10 +60,12 @@ window.onload = function () {
         panel.id = item.id;
         panel.description = item.description;
         panel.imgPath = "source/" + item.id + ".jpg";
-        panel.div = addElementsCards(panel);
+        panel.div = addElementsCards('div:p,img,span',panel);
         panel.display = true;
         panelArr.push(panel);
     });
+
+
 
     // let html = document.documentElement;
     // if (html.attachEvent) {
@@ -40,7 +76,8 @@ window.onload = function () {
     // }
 }
 
-addElementsCards = (elementObj) => {
+addElementsPanel = (structure, elementObj) => {
+    structure.split
     let divPanel = document.createElement('div');
     let imgBackground = document.createElement('img');
     let p = document.createElement('p');
@@ -82,6 +119,7 @@ panelClick = (event) => {
             item.display = true;
 
         classMover(item.div, item.display);
+        addElementsCards()
         descriptionDiv.style.display = 'block';
 
         if (!isClicked) {
