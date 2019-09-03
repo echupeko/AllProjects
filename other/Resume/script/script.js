1e785f010c0e04d47bdab82128b2f2b26ac63652
Евгений <echupeko@gmail.com>
2019-09-03 16:35:32 +0700

    update main site

--- other/Resume/script/script.js
+++ other/Resume/script/script.js
@@ -1,10 +1,13 @@
 const headerBlock = document.querySelector('header');
+const aboutMe = document.getElementById('about-me');
 
 window.onload = function () {
     menuListAdd();
 }
 
 window.onscroll = function () {
+    let navButton = document.getElementById('nav-btn');
+
     if (pageYOffset > 80) {
         headerBlock.classList.add('header-fixed');
     }
@@ -12,21 +15,25 @@ window.onscroll = function () {
         headerBlock.classList.remove('header-fixed');
     }
 
-    let menuCoords = headerBlock.offsetHeight + 100;
+    if(pageYOffset > 400) {
+        navButton.style.opacity = '1';
+    }
+    else {
+        navButton.style.opacity = '0';
+    }
 
-    if (menuCoords < pageYOffset) {
-        navButtonElem.style.opacity = '1';
-        navButtonElem.style.cursor = 'pointer';
-    } else {
-        navButtonElem.style.opacity = '0';
-        navButtonElem.style.cursor = 'auto';
+    if(pageYOffset > 600) {
+        aboutMe.id = 'unFocus-about-me';
+    }
+    else {
+        aboutMe.id = 'about-me';
     }
 };
 
 const menuListAdd = () => {
     let li = "";
     contentArr.forEach(item => {
-        li += '<li><a class="linkMenu" href="">' + item.description + '</a></li>';
+        li += '<li><a class="linkMenu" href="#' + item.id + '">' + item.description + '</a></li>';
         return li;
     });
     headerBlock.innerHTML = `<ul> ${li} </ul>`;
