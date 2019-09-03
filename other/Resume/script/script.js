4ef86e11591c133f15b501100e850e7b6d99b64c
Евгений <echupeko@gmail.com>
2019-09-03 13:26:59 +0700

    update main site

--- other/Resume/script/script.js
+++ other/Resume/script/script.js
@@ -4,11 +4,24 @@ window.onload = function () {
     menuListAdd();
 }
 
+window.onscroll = function () {
+    let menuCoords = menuElem.offsetHeight + 100;
+
+    if (menuCoords < pageYOffset) {
+        navButtonElem.style.opacity = '1';
+        navButtonElem.style.cursor = 'pointer';
+    } else {
+        navButtonElem.style.opacity = '0';
+        navButtonElem.style.cursor = 'auto';
+    }
+};
+
 const menuListAdd = () => {
     let li = "";
     contentArr.forEach(item => {
-        li += '<li>' + item.description + '</li>';
+        li += '<li><a class="linkMenu" href="">' + item.description + '</a></li>';
         return li;
     });
     headerBlock.innerHTML = `<ul> ${li} </ul>`;
 }
+
