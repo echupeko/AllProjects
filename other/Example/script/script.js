365b4f535370ad8f6fe2cc1422b41b44ea2717db
Dell <echupeko@gmail.com>
2019-09-11 22:16:08 +0700

    update CardGame

--- other/Example/script/script.js
+++ other/Example/script/script.js
@@ -4,7 +4,8 @@ window.onload = () => {
     as.style.height = 2 * screen.height + 'px';
 }
 window.onmousemove = () => {
-    let y = -Math.round((event.clientX - screen.width / 2) / 360,2);
+    as = document.getElementById('as');
+    let y = -Math.round((event.clientX - screen.width / 2) / 270,2);
     let x = Math.round((event.clientY - screen.height / 2) / 90,2);
     as.style.transform = 'rotateY(' + y + 'deg) rotateX(' + x + 'deg)';
 }
