f068ea60eb943fffea714f1e6de21bad50fa79d8
Евгений <echupeko@gmail.com>
2019-09-12 16:49:41 +0700

    update main site

--- other/Example/script/script.js
+++ other/Example/script/script.js
@@ -1,30 +1,34 @@
 let as;
 let isColor = false;
 window.onload = () => {
-    as = document.getElementById('as');
+    as = document.getElementById('network-background');
     as.style.height = 2 * screen.height + 'px';
 }
 window.onmousemove = () => {
-    as = document.getElementById('as');
+    as = document.getElementById('network-background');
     let y = -Math.round((event.clientX - screen.width / 2) / 270, 2);
     let x = Math.round((event.clientY - screen.height / 2) / 90, 2);
     as.style.transform = 'rotateY(' + y + 'deg) rotateX(' + x + 'deg)';
 }
+window.onmousewheel = () => {
+   bwTheme();
 
-const clik = () => {
-    as = document.getElementById('as');
+}
+
+const bwTheme = () => {
+    as = document.getElementById('network-background');
     document.querySelector('body').style.backgroundColor = isColor ? '#1c1c1c' : 'white';
     as.classList.add(isColor ? 'black' : 'white');
     as.classList.remove(isColor ? 'white' : 'black');
     if (!isColor) {
-        document.getElementById('btn').classList.add('activate');
+        document.getElementById('switch').classList.add('activate');
         let li = document.querySelectorAll('li');
         for (let i=0;i<li.length;i++){
             li[i].classList.add('activate');
         }
     }
     else {
-        document.getElementById('btn').classList.remove('activate');
+        document.getElementById('switch').classList.remove('activate');
         let li = document.querySelectorAll('li');
         for (let i=0;i<li.length;i++){
             li[i].classList.remove('activate');
