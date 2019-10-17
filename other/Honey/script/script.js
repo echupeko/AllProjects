e71fa2af3fbbbccc530622e1a304b3d0eb96dd35
Евгений <echupeko@gmail.com>
2019-10-17 17:00:10 +0700

    update main site

--- other/Honey/script/script.js
+++ other/Honey/script/script.js
@@ -1,22 +1,28 @@
-function handleFileSelect(evt) {
-    let workbook = XLSX.read(data, {type: 'binary'});
-    //Get the files from Upload control
-    let first_sheet_name = workbook.SheetNames[0];
-    let files = evt.target.files;
-    let i, f;
-    //Loop through files
-    for (let i = 0, f = files[i]; i != files.length; ++i) {
-        let reader = new FileReader();
-        let name = f.name;
-        reader.onload = function (evt) {
-            let data = evt.target.result;
+// function handleFileSelect(evt) {
+//     let workbook = XLSX.read(data, {type: 'binary'});
+//     //Get the files from Upload control
+//     let first_sheet_name = workbook.SheetNames[0];
+//     let files = evt.target.files;
+//     let i, f;
+//     //Loop through files
+//     for (let i = 0, f = files[i]; i != files.length; ++i) {
+//         let reader = new FileReader();
+//         let name = f.name;
+//         reader.onload = function (evt) {
+//             let data = evt.target.result;
+//
+//             let result;
+//             /* convert from workbook to array of arrays */
+//             let first_worksheet = data.Sheets[data.SheetNames[0]];
+//             data = XLSX.utils.sheet_to_json(first_worksheet, {header:1});
+//             alert(result[0].Column1);
+//         };
+//         reader.readAsArrayBuffer(f);
+//     }
+// }
+let ct = 0;
 
-            let result;
-            /* convert from workbook to array of arrays */
-            let first_worksheet = data.Sheets[data.SheetNames[0]];
-            data = XLSX.utils.sheet_to_json(first_worksheet, {header:1});
-            alert(result[0].Column1);
-        };
-        reader.readAsArrayBuffer(f);
-    }
+const addOrder = () => {
+    ct++;
+    document.getElementById('numberProduct').innerText = ct;
 }
