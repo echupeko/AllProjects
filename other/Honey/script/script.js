0337f251a4f7b5d53293536e095d3416462facb2
Евгений <echupeko@gmail.com>
2019-10-22 16:57:17 +0700

    update main site

--- other/Honey/script/script.js
+++ other/Honey/script/script.js
@@ -21,10 +21,55 @@
 //     }
 // }
 let ct = 0;
+let orderList = [];
+let order = {};
+
+
+window.onload = () => {
+    const mainBlock = document.getElementById('contentBlock');
+    let content = "";
+    honeyList.forEach(item => {
+        content += '<div class="block">' +
+            '<div class="info">' +
+            '   <h3>Мёд ' + item.name + ' ' + item.count + 'л.</h3>' +
+            '   <img src="source/bochka.png">' +
+            '   <p class="description-honey">' + item.description + '</p>' +
+            '   <p>цена: ' + item.price + ' руб.</p>' +
+            '</div>' +
+            '<div class="control">' +
+            '   <div>' +
+            '       <input name="' + item.name + item.count + '" type="submit" class="miniBtn" value="-" ' +
+            '           onclick="countHoney(-1,\'' + item.id + item.count + '\')">' +
+            '       <input id="' + item.id + item.count + '" name="' + item.name + item.count + '" step="1" ' +
+            '           min="' + item.min + '" max="' + item.max + '" onkeyup="countHoney(0)" type="number" placeholder="1">' +
+            '       <input name="' + item.name + item.count + '" type="submit" class="miniBtn" value="+" ' +
+            '           onclick="countHoney(1,\'' + item.id + item.count + '\')">' +
+            '   </div>' +
+            '   <input class="button" type="submit" value="Добавить к заказу" onclick="addOrder()">' +
+            '</div></div>';
+
+    });
+    mainBlock.innerHTML += content;
+}
+const countHoney = (pre, id) => {
+    let element = document.getElementById(id);
+    let selected = orderList.indexOf(orderList.find(item => item.id.toString() === id.toString()));
+    if (selected >= 0){
+        order = orderList[selected];
+        order.count += pre;
+        element.value = order.count;
+    }
+    else {
+        order.id = id;
+        order.count = pre;
+        orderList.push(order);
+        element.value = order.count;
+    }
+};
 
 const addOrder = () => {
-    ct++;
-
-    document.getElementById('numberProduct').style.opacity = 1;
-    document.getElementById('numberProduct').innerText = ct;
-}
+    // ct++;
+    //
+    // document.getElementById('numberProduct').style.opacity = 1;
+    // document.getElementById('numberProduct').innerText = ct;
+};
