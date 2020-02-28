b103a29242fc930c2867e661390b062db98dcb05
Евгений <echupeko@gmail.com>
2020-02-28 13:45:04 +0700

    update main site

--- other/Tutorial/script/script.js
+++ other/Tutorial/script/script.js
@@ -1,14 +1,20 @@
-var app = new Vue({
+let app = new Vue({
     el: "#navBar",
     data: {
         title: "Hello World!",
-        countOrder: 0
+        countOrder: 0,
+        imageSrc: "resource/logo.png"
     },
     methods: {
         revers: function () {
             this.countOrder++;
         },
-
+        imgOver: function () {
+            this.imageSrc = "resource/logo_hover.png"
+        },
+        imgOut: function () {
+            this.imageSrc = "resource/logo.png"
+        }
     }
 });
 
