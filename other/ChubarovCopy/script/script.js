cf98d467196d8447285b98b14761e5e0399a8b2b
Евгений <echupeko@gmail.com>
2020-06-29 12:15:48 +0700

    update main site

---
+++ other/ChubarovCopy/script/script.js
@@ -0,0 +1,80 @@
+let visibleMenu = 1;
+const btnToUp = document.getElementById('btn-to-up');
+let orderItem = {
+    id: 0,
+    honey: 0,
+    count: 0,
+    inBasket: false
+};
+let basketOrder = [];
+
+window.onload = function () {
+    honeyList.forEach(item => {
+        if(item.sale) {
+            hotBlock.title = item.name;
+            hotBlock.oldPrice = item.price;
+            hotBlock.sale = item.salePrice;
+            hotBlock.count = item.count;
+            hotBlock.honey = item.id;
+        }
+    })
+
+    document.getElementById('basketForm').style.display = 'none';
+    document.getElementById('basketForm').style.minHeight = clientHeight - 72 + 'px';
+}
+
+window.onscroll = function () {
+    if (pageYOffset > 80) {
+        document.querySelector('header').classList.add('header-fixed');
+        // document.getElementById('basketIco').style.width = '30px';
+        // document.getElementById('logo').style.top = '22px';
+    }
+    else {
+        document.querySelector('header').classList.remove('header-fixed');
+        // document.getElementById('basketIco').style.width = '50px';
+        // document.getElementById('logo').style.top = '0';
+    }
+
+    if (pageYOffset > 400) {
+        btnToUp.style.opacity = '1';
+        btnToUp.style.cursor = 'pointer';
+    }
+    else {
+        btnToUp.style.opacity = '0';
+        btnToUp.style.cursor = 'auto';
+    }
+}
+
+let menuBar = () => {
+    const menu = document.getElementById('menu');
+    if (window.matchMedia('(max-width: 1010px)').matches) {
+        if (visibleMenu) {
+            menu.classList.add('active');
+            visibleMenu--;
+        }
+        else {
+            menu.classList.toggle('active');
+            visibleMenu++;
+        }
+    }
+}
+
+// window.onmousemove = () => {
+//     as = document.getElementById('qa');
+//     let y = event.clientX;
+//     let x = event.clientY;
+//     as.style.top = x +'px';
+//     as.style.left = y +'px';
+// }
+
+let addedOrder = (id) => {
+    orderItem.id = basketOrder.length;
+    orderItem.honey = id;
+    orderItem.count = document.getElementById('honey' + id).value;
+    orderItem.inBasket = !orderItem.inBasket;
+    basketOrder.push(orderItem);
+}
+
+let addedInBasket = (order) => {
+
+}
---
+++ other/ChubarovCopy/script/vue.js
@@ -0,0 +1,133 @@
+let quantityGlobal = 0;
+let amountGlobal = 0;
+let clientHeight = window.innerHeight;
+
+
+// let myMixin = {
+//     created: function () {
+//         this.addedOrder();
+//         this.addedInBasket();
+//     },
+//     methods: {
+//         addedOrder: function (id, count) {
+//             orderItem.honey = id;
+//             orderItem.count = count;
+//         },
+//         addedInBasket: function (order) {
+//             basketOrder.push(order)
+//         }
+//     }
+// }
+
+let navBar = new Vue({
+    el: "#navBar",
+    data: {
+        visibleForm: false,
+        amount: 'Корзина пуста',
+        quantity: 0,
+        visibleQuantity: 'hidden',
+        sizeQuantity: '14px',
+        topQuantity: '-2px',
+        leftQuantity: '4px',
+        logoSrc: 'resource/logo.png',
+        basketSrc: 'resource/basket.png',
+        navItems: navList
+    },
+    methods: {
+        addedAmount(sal) {
+            quantityGlobal++;
+            amountGlobal += sal;
+            this.quantity = quantityGlobal;
+            this.amount = 'Заказ на ' + amountGlobal + 'руб.';
+            if (this.amount) {
+                this.visibleQuantity = 'visible'
+            }
+            if(this.quantity > 9){
+                this.sizeQuantity = '12px';
+                this.topQuantity = '-13px';
+                this.leftQuantity = '1px';
+            }
+        },
+        scrollingTo(to) {
+            let topElem = document.getElementById(to).offsetTop-((pageYOffset > 100)? 70 : 142);
+            window.scrollTo({
+                top: topElem,
+                behavior: 'smooth'
+            });
+        },
+        openBasket() {
+            this.visibleForm = !this.visibleForm;
+            document.getElementById('basketForm').style.display = (this.visibleForm)? 'flex' : 'none';
+            document.getElementById('main').style.display = (!this.visibleForm)? 'block' : 'none';
+        }
+    }
+});
+
+
+
+let hotBlock = new Vue({
+    el: "#hotBlock",
+    data: {
+        title: '',
+        oldPrice: 0,
+        sale: 0,
+        count: 0,
+        id: 0
+    },
+    methods: {
+        addedQuantity() {
+            navBar.addedAmount(this.sale);
+        }
+    }
+});
+
+Vue.component('item', {
+    props: ['cat'],
+    template: '' +
+        '<div class="catalogItem flex-display">' +
+        '   <h3>Мёд {{cat.name}} {{cat.count}} л.</h3>' +
+        '   <img src="resource/bochka.png">' +
+        '   <p>цена: {{cat.price}} руб.</p>' +
+        '   <div class="flex-display row">' +
+        '       <input class="input" type="submit" value="-" @click="handleClick">' +
+        '       <input v-bind:id="\'honey\' + cat.id" class="input" step="1" v-model="count" min="1" max="20" type="number">' +
+        '       <input class="input" type="submit" value="+" @click="handleClick">' +
+        '   </div>' +
+        '   <input class="btn" type="submit" value="Добавить к заказу"  v-bind:onclick="addedOrder(cat.id)">' +
+        '</div>',
+    methods: {
+        handleClick() {
+            this.$emit('click');
+        }
+    }
+})
+
+let catalogBlock = new Vue({
+    el: "#catalogBlock",
+    data: {
+        title: 'catalogItem',
+        honeyVueList: honeyList,
+        count: 0
+    },
+    methods: {
+        orderAdd : function(id) {
+            navBar.addedAmount(honeyList[id].price);
+            basketOrder.push(honeyList[id]);
+        }
+    }
+});
+
+let basketCatalog = new Vue({
+    el: "#basketForm",
+    data: {
+        title: 'basketItem',
+        honeyVueList: basketOrder
+    }
+})
+//
+//
+// let Component = Vue.extend({
+//     mixins: [myMixin]
+// });
+//
+// let component = new Component();
