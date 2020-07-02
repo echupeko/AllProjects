d94111a49a1aa4ea43b9e71d26ddb85708c60bc2
Евгений <echupeko@gmail.com>
2020-07-02 11:21:49 +0700

    update main site

--- other/ChubarovCopy/script/vue.js
+++ other/ChubarovCopy/script/vue.js
@@ -43,14 +43,14 @@ let navBar = new Vue({
             if (this.amount) {
                 this.visibleQuantity = 'visible'
             }
-            if(this.quantity > 9){
+            if (this.quantity > 9) {
                 this.sizeQuantity = '12px';
                 this.topQuantity = '-13px';
                 this.leftQuantity = '1px';
             }
         },
         scrollingTo(to) {
-            let topElem = document.getElementById(to).offsetTop-((pageYOffset > 100)? 70 : 142);
+            let topElem = document.getElementById(to).offsetTop - ((pageYOffset > 100) ? 70 : 142);
             window.scrollTo({
                 top: topElem,
                 behavior: 'smooth'
@@ -58,13 +58,32 @@ let navBar = new Vue({
         },
         openBasket() {
             this.visibleForm = !this.visibleForm;
-            document.getElementById('basketForm').style.display = (this.visibleForm)? 'flex' : 'none';
-            document.getElementById('main').style.display = (!this.visibleForm)? 'block' : 'none';
+            document.getElementById('basketForm').style.display = (this.visibleForm) ? 'flex' : 'none';
+            document.getElementById('main').style.display = (!this.visibleForm) ? 'block' : 'none';
         }
     }
 });
 
+Vue.component('slider-item', {
+    props: ['slide'],
+    template: '' +
+    '<div class="carousel-item" :class="{\'active\': !slide.id}">' +
+    '   <img :src="\'resource/slider_\'+slide.id+\'.png\'" class="d-block w-100" :alt="slide.title">' +
+    '   <div class="carousel-caption d-flex flex-column align-items-center">' +
+    '       <h5>{{slide.title}}</h5>' +
+    '       <p>{{slide.description}}</p>' +
+    '   </div>' +
+    '</div>'
+})
 
+let carousel = new Vue({
+    el: "#carouselExampleCaptions",
+    data: {
+        isActive: true,
+        title: 'sliderItems',
+        sliderList: carouselList
+    }
+});
 
 let hotBlock = new Vue({
     el: "#hotBlock",
@@ -85,17 +104,17 @@ let hotBlock = new Vue({
 Vue.component('item', {
     props: ['cat'],
     template: '' +
-        '<div class="catalogItem flex-display">' +
-        '   <h3>Мёд {{cat.name}} {{cat.count}} л.</h3>' +
-        '   <img src="resource/bochka.png">' +
-        '   <p>цена: {{cat.price}} руб.</p>' +
-        '   <div class="flex-display row">' +
-        '       <input class="input" type="submit" value="-" @click="handleClick">' +
-        '       <input v-bind:id="\'honey\' + cat.id" class="input" step="1" v-model="count" min="1" max="20" type="number">' +
-        '       <input class="input" type="submit" value="+" @click="handleClick">' +
-        '   </div>' +
-        '   <input class="btn" type="submit" value="Добавить к заказу"  v-bind:onclick="addedOrder(cat.id)">' +
-        '</div>',
+    '<div class="catalogItem flex-display">' +
+    '   <h3>Мёд {{cat.name}} {{cat.count}} л.</h3>' +
+    '   <img src="resource/bochka.png">' +
+    '   <p>цена: {{cat.price}} руб.</p>' +
+    '   <div class="flex-display row">' +
+    '       <input class="input" type="submit" value="-" @click="handleClick">' +
+    '       <input v-bind:id="\'honey\' + cat.id" class="input" step="1"  min="1" max="20" type="number">' +
+    '       <input class="input" type="submit" value="+" @click="handleClick">' +
+    '   </div>' +
+    '   <input class="btn" type="submit" value="Добавить к заказу">' +
+    '</div>',
     methods: {
         handleClick() {
             this.$emit('click');
@@ -108,23 +127,22 @@ let catalogBlock = new Vue({
     data: {
         title: 'catalogItem',
         honeyVueList: honeyList,
-        count: 0
     },
     methods: {
-        orderAdd : function(id) {
+        orderAdd: function (id) {
             navBar.addedAmount(honeyList[id].price);
             basketOrder.push(honeyList[id]);
         }
     }
 });
-
-let basketCatalog = new Vue({
-    el: "#basketForm",
-    data: {
-        title: 'basketItem',
-        honeyVueList: basketOrder
-    }
-})
+//
+// let basketCatalog = new Vue({
+//     el: "#basketForm",
+//     data: {
+//         title: 'basketItem',
+//         honeyVueList: basketOrder
+//     }
+// })
 //
 //
 // let Component = Vue.extend({
