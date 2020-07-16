b249cf7e4b44ca9d93e9a8d0e54a2874679305be
Евгений <echupeko@gmail.com>
2020-07-16 12:13:12 +0700

    update main site

--- other/ChubarovCopy/script/vue.js
+++ other/ChubarovCopy/script/vue.js
@@ -44,13 +44,13 @@ let navBar = new Vue({
 Vue.component('slider-item', {
     props: ['slide'],
     template: '' +
-        '<div class="carousel-item h-100" :class="{\'active\': !slide.id}">' +
-        '   <img :src="\'resource/slider_\'+slide.id+\'.png\'" class="d-block h-100" :alt="slide.title">' +
-        '   <div class="carousel-caption d-flex flex-column align-items-center">' +
-        '       <h5>{{slide.title}}</h5>' +
-        '       <p>{{slide.description}}</p>' +
-        '   </div>' +
-        '</div>'
+    '<div class="carousel-item h-100" :class="{\'active\': !slide.id}">' +
+    '   <img :src="\'resource/slider_\'+slide.id+\'.png\'" class="d-block h-100" :alt="slide.title">' +
+    '   <div class="carousel-caption d-flex flex-column align-items-center">' +
+    '       <h5>{{slide.title}}</h5>' +
+    '       <p>{{slide.description}}</p>' +
+    '   </div>' +
+    '</div>'
 })
 
 let carousel = new Vue({
@@ -78,27 +78,64 @@ let hotBlock = new Vue({
     }
 });
 
+// Vue.component('item', {
+//     props: ['cat'],
+//     template: `
+//         <div class="card d-flex flex-column justify-content-center align-items-center" style="width: 18rem;">
+//            <h4 class="card-title">Мёд {{cat.name}} {{cat.count}} л.</h4>
+//            <img src="resource/bochka.png" class="card-img-top" v-bind:alt="'Мёд' + cat.name">
+//            <div class="card-body d-flex flex-column justify-content-center align-items-center">
+//                <div class="d-flex flex-column">
+//                <p class="card-text">{{cat.price}} Р.</p>
+//                    <!--<div class="d-flex flex-row w-100">-->
+//                        <!--<input class="input" type="submit" value="-" @click="uppp(cat.id,-1)">-->
+//                        <!--<input v-bind:id="'honey' + cat.id" class="input" step="1"  min="1" max="20" type="number" v-model="cat.order">-->
+//                        <!--<input class="input" type="submit" value="+"  @click="uppp(cat.id, 1)">-->
+//                    <!--</div>-->
+//                    <a class="btn btn-warning" @click="$emit('click', cat.id)">В корзину</a>
+//                </div>
+//            </div>
+//         </div>`,
+//     methods: {
+//         uppp: function (id, count) {
+//             this.$emit('up-down', id, count)
+//         }
+//     }
+// })
+
 Vue.component('item', {
     props: ['cat'],
     template: `
         <div class="card d-flex flex-column justify-content-center align-items-center" style="width: 18rem;">
-           <h4 class="card-title">Мёд {{cat.name}} {{cat.count}} л.</h4>
+           <h4 class="card-title">Мёд {{cat.name}}</h4>
            <img src="resource/bochka.png" class="card-img-top" v-bind:alt="'Мёд' + cat.name">
            <div class="card-body d-flex flex-column justify-content-center align-items-center">
-               <p class="card-text">цена: {{cat.price}} руб.</p>
-               <div class="d-flex flex-column">
-                   <div class="d-flex flex-row w-100">
-                       <input class="input" type="submit" value="-" @click="uppp(cat.id,-1)">
-                       <input v-bind:id="'honey' + cat.id" class="input" step="1"  min="1" max="20" type="number" v-model="cat.order">
-                       <input class="input" type="submit" value="+"  @click="uppp(cat.id, 1)">
-                   </div>
-                   <a class="btn btn-warning" @click="$emit('click', cat.id)">Добавить к заказу</a>
-               </div>
+                <div class="slide-bar w-100 d-flex flex-row justify-content-sm-between align-items-center" >
+                    <div class="slide-point" ></div>
+                    <div v-for="slideItem in cat.count" class="slide-item" @click="sliders(cat.id, cat.count.indexOf(slideItem))">
+                        {{slideItem}} л.
+                    </div>
+                </div>
+                <div class="d-flex flex-row">
+                    <p class="card-text">{{cat.price}} Р.</p>
+                    <a class="btn btn-warning" @click="$emit('click', cat.id)">В корзину</a>
+                </div>
            </div>
         </div>`,
+    mounted: function() {
+        var slideBar = document.getElementsByClassName('slide-bar')[this.cat.id];
+        slideBar.getElementsByClassName('slide-point')[0].style.width = 100/this.cat.count.length + "%";
+        let itemList = slideBar.getElementsByClassName('slide-item');
+        for(let i=0; i<itemList.length; i++){
+             itemList[i].style.width = 100/this.cat.count.length + "%";
+        }
+    },
     methods: {
-        uppp: function (id, count) {
-            this.$emit('up-down', id, count)
+        // uppp: function (id, count) {
+        //     this.$emit('up-down', id, count)
+        // },
+        sliders: function (id, count) {
+            this.$emit('slide-to', id, count)
         }
     }
 })
@@ -107,8 +144,7 @@ let catalogBlock = new Vue({
     el: "#catalogBlock",
     data: {
         title: 'catalogItem',
-        honeyVueList: honeyList,
-
+        honeyVueList: catalogList,
     },
     methods: {
         orderAdd: function (id) {
@@ -119,12 +155,14 @@ let catalogBlock = new Vue({
                     return;
                 }
             }
-
             navBar.addedAmount(honeyList[id].price * honeyList[id].order);
             basketOrder.push(honeyList[id]);
         },
         upDownCount: function (id, c) {
             this.honeyVueList[id].order += c;
+        },
+        slideTo: function (id, count) {
+            document.getElementsByClassName('slide-point')[id].style.transform = "translatex("+(100*count)+"%)";
         }
     }
 });
